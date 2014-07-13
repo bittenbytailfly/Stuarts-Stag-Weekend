62a6abd07e4dd2c31f520b077a5c5711490e4cbe
Ed Carter <bittenbytailfly@server.fake>
2014-07-14 00:32:15 +0100

    First commit - completely raw.

---
+++ static/js/controllers.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+angular.module('superheroSelector', ['ui.bootstrap'])
+
+.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[{');
+    $interpolateProvider.endSymbol('}]}');
+})
+
+.controller('selectTypeCtrl', function ($scope, $http) {
+
+    $scope.test = 'hello';
+
+    $http.post('/ajax/availableTypes')
+        .success(function (data) {
+            $scope.heroes = data.heroes;
+            $scope.villains = data.villains;
+        });
+});
